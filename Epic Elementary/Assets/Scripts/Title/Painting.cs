0b81e32a375d77aa9663187d8b3a2f4687f98131
Voltaire Stephen Medina <voltmedina@gmail.com>
2016-09-09 13:31:37 -0400

    Added paint splash animation to title screen

---
+++ Epic Elementary/Assets/Scripts/Title/Painting.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Painting : MonoBehaviour {
+
+    [SerializeField]
+    public GameObject paintBall;
+    public Animator paintBallAnim;
+
+    public GameObject splatter;
+
+	void Start () {
+
+        paintBallAnim = paintBall.GetComponent<Animator>();
+
+        splatter = this.gameObject;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+        if (paintBallAnim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 ) {
+
+            splatter.GetComponent<SpriteRenderer>().enabled = true;
+            paintBall.GetComponentInChildren<MeshRenderer>().enabled = false;
+
+        }
+	}
+
+
+}
