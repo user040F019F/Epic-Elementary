c1d81e3a077e30d55511beeb79fb98b83d4e9c1b
Voltaire Stephen Medina <voltmedina@gmail.com>
2016-09-09 22:28:02 -0400

    (Title update 4) finished all animations, and sound effects, still need resolution fixes

---
+++ Epic Elementary/Assets/Scripts/Title/ChalkSound.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChalkSound : MonoBehaviour {
+
+    [SerializeField]
+    public int seconds;
+
+    void Start() {
+        StartCoroutine(Trim());
+    }
+
+    IEnumerator Trim() {
+        this.GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(seconds);
+        this.GetComponent<AudioSource>().Stop();
+    }
+
+}
---
+++ Epic Elementary/Assets/Scripts/Title/ThudSoundTrim.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThudSoundTrim : MonoBehaviour {
+
+    [SerializeField]
+    public int seconds;
+
+    void Start() {
+        StartCoroutine(Trim());
+    }
+
+    IEnumerator Trim() {
+
+        yield return new WaitForSeconds(seconds);
+        this.GetComponent<AudioSource>().Play();
+    }
+
+}
