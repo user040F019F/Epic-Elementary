d72d0bbfb77ec00c79e3bd0c2a2ab95b6ac42b78
Voltaire Stephen Medina <voltmedina@gmail.com>
2016-09-19 22:37:51 -0400

    Added button functionality on main menu and a base animation for chalk writing (not final)

---
+++ Epic Elementary/Assets/Scripts/Title/HighlightButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class HighlightButton : MonoBehaviour, IPointerEnterHandler, ISelectHandler {
+
+    private bool hovering;
+    
+    public void OnPointerEnter(PointerEventData eventData) {
+        Debug.Log("Working");
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        Debug.Log("Working2");
+    }
+
+    public void OnSelect(BaseEventData eventData) {
+        //do your stuff when selected
+    }
+}
---
+++ Epic Elementary/Assets/Scripts/Title/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    public string startLevel;
+
+    public string instructions;
+
+    public string mainMenu;
+
+    public string credits;
+
+    public void NewGame() {
+        Application.LoadLevel(startLevel);
+        Destroy(GameObject.FindWithTag("MusicManager"));
+    }
+
+    public void Instructions() {
+        Application.LoadLevel(instructions);
+    }
+
+    public void Menu() {
+        Application.LoadLevel(mainMenu);
+    }
+
+}
