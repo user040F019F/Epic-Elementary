b363643fb2a708bab65dc0beb40732f4f5ccdbdd
Voltaire Stephen Medina <voltmedina@gmail.com>
2016-10-10 10:56:58 -0400

    Imported Player sound effects, created sound effect manager, began scripting player's sound effects

---
+++ Epic Elementary/Assets/Scripts/Level/Other/SoundEffectsManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundEffectsManager : MonoBehaviour {
+
+    public AudioSource clip;
+
+	// Use this for initialization
+	void Start () {
+        clip = this.GetComponent<AudioSource>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0)) {
+
+            clip.Play();
+
+            Debug.Log("working");
+
+        }
+
+    }
+}
