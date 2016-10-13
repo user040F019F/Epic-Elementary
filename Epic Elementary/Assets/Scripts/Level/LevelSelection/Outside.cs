377d1c9b4e062ed7235a5004a12fb64abf13f549
Voltaire Stephen Medina <voltmedina@gmail.com>
2016-10-13 11:39:22 -0400

    Updates in level select: Added outside stage/gate , Added some small decorations

---
+++ Epic Elementary/Assets/Scripts/Level/LevelSelection/Outside.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Outside : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other) {
+
+        if (other.tag == "Actor") {
+
+            Debug.Log("Entering Outside");
+            Application.LoadLevel("Level");
+
+
+        }
+    
+    }
+
+}
