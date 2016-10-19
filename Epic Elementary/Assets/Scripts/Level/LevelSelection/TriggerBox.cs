75a8b1c7c7e1d06dc5c911268d0461251f4e72e4
Voltaire Stephen Medina <voltmedina@gmail.com>
2016-10-18 22:30:30 -0400

    Level Select updates: Implemented walking text annotations for areas!,  Achivement display, more textures/objects/decor, new lighting

---
+++ Epic Elementary/Assets/Scripts/Level/LevelSelection/TriggerBox.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerBox : MonoBehaviour {
+
+    public SpriteRenderer sprite;
+    public float fadeSpeed;
+    private float tempAlphaColor;
+
+    private bool fade = true;
+    
+	
+	void Start () {
+        tempAlphaColor = sprite.color.a;
+    }
+	
+    void OnTriggerEnter (Collider other) {
+
+        if (other.tag == "Actor") {         
+
+            fade = false;
+
+        }
+
+    }
+
+    void OnTriggerExit(Collider other) {
+
+        if (other.tag == "Actor") {
+           
+            fade = true;
+
+        }
+
+    }
+
+    void Update () {
+
+        sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, tempAlphaColor);
+
+        if (fade == false) {
+            IncreaseAlpha();
+        }
+
+        if (fade == true) {
+            DecreaseAlpha();
+        }
+
+	}
+
+
+
+    void IncreaseAlpha () {
+
+        if (tempAlphaColor != 2) {
+            tempAlphaColor += fadeSpeed;
+        }
+
+        // cap the alpha value
+        if (tempAlphaColor > 1) {
+            tempAlphaColor = 1;
+        }
+    }
+
+    void DecreaseAlpha () {
+
+        if (tempAlphaColor != 0) {
+            tempAlphaColor -= fadeSpeed;
+        }
+
+        // cap the alpha value
+        if (tempAlphaColor < -1) {
+            tempAlphaColor = -1;
+        }
+    }
+
+
+}
